0532c1310d1abeec2f4dcd300e9ae478e7b87b7e
Yuichi Sugiyama <yuichis@ricsec.co.jp>
2024-10-08 06:40:59 +0000

    core/lib/qcbor: add build configuration for QCBOR library
    This commit adds the necessary build configuration for integrating the
    QCBOR library. Updates to core.mk ensure that the library is included
    when CFG_QCBOR is enabled. A sub.mk file is also added to define the
    source files and global include directories for QCBOR.
    
    Signed-off-by: Yuichi Sugiyama <yuichis@ricsec.co.jp>

---
+++ core/lib/qcbor/sub.mk
@@ -0,0 +1,10 @@
+global-incdirs-y += inc
+cflags-y += -Wno-declaration-after-statement
+cflags-y += -Wno-redundant-decls
+cflags-y += -DQCBOR_DISABLE_FLOAT_HW_USE
+cflags-remove-y += -mgeneral-regs-only
+srcs-y += src/ieee754.c
+srcs-y += src/qcbor_decode.c
+srcs-y += src/qcbor_encode.c
+srcs-y += src/qcbor_err_to_str.c
+srcs-y += src/UsefulBuf.c
