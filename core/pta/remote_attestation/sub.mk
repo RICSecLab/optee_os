a69ee2497c33fc9b2aeee8c50aadd879e181e24e
Yuichi Sugiyama <yuichis@ricsec.co.jp>
2024-10-11 03:21:55 +0000

    clean up

--- core/pta/remote_attestation/sub.mk
+++ core/pta/remote_attestation/sub.mk
@@ -1,4 +1,4 @@
-srcs-$(CFG_REMOTE_ATTESTATION_PTA) += remote_attestation.c
-srcs-$(CFG_REMOTE_ATTESTATION_PTA) += cbor.c
-srcs-$(CFG_REMOTE_ATTESTATION_PTA) += hash.c
-srcs-$(CFG_REMOTE_ATTESTATION_PTA) += sign.c
+srcs-$(CFG_VERAISON_ATTESTATION_PTA) += remote_attestation.c
+srcs-$(CFG_VERAISON_ATTESTATION_PTA) += cbor.c
+srcs-$(CFG_VERAISON_ATTESTATION_PTA) += hash.c
+srcs-$(CFG_VERAISON_ATTESTATION_PTA) += sign.c
