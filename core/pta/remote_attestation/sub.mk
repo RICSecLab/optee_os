6c9700f991dd86711ca1066f1ef7f61a3270868e
Yuichi Sugiyama <yuichis@ricsec.co.jp>
2024-10-11 03:58:07 +0000

    core/pta/remote_attestation: rename configuration option for remote attestation PTA
    Updated the build configuration to replace CFG_REMOTE_ATTESTATION_PTA with
    CFG_VERAISON_ATTESTATION_PTA in sub.mk. This aligns the configuration option
    with the new naming convention for the Veraison attestation PTA.
    
    Signed-off-by: Yuichi Sugiyama <yuichis@ricsec.co.jp>

--- core/pta/remote_attestation/sub.mk
+++ core/pta/remote_attestation/sub.mk
@@ -1,5 +1,5 @@
-srcs-$(CFG_REMOTE_ATTESTATION_PTA) += remote_attestation.c
-srcs-$(CFG_REMOTE_ATTESTATION_PTA) += base64.c
-srcs-$(CFG_REMOTE_ATTESTATION_PTA) += cbor.c
-srcs-$(CFG_REMOTE_ATTESTATION_PTA) += hash.c
-srcs-$(CFG_REMOTE_ATTESTATION_PTA) += sign.c
+srcs-$(CFG_VERAISON_ATTESTATION_PTA) += remote_attestation.c
+srcs-$(CFG_VERAISON_ATTESTATION_PTA) += base64.c
+srcs-$(CFG_VERAISON_ATTESTATION_PTA) += cbor.c
+srcs-$(CFG_VERAISON_ATTESTATION_PTA) += hash.c
+srcs-$(CFG_VERAISON_ATTESTATION_PTA) += sign.c
